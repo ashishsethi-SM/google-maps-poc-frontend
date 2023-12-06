9298b0d096dc1ba9ce944f78b2aa28b94c387b75
Ashish Sethi <ashish.sethi@sociometrik.com>
2023-12-06 11:15:26 +0530

    fix: fitbounds used

--- src/components/SampleControls.tsx
+++ src/components/SampleControls.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import heatMapData from '../mockData/heatMapData.json'
 import { useSelector } from 'react-redux'
+import * as turf from '@turf/turf'
 import { MapState } from '@/store/slicers/mapSlice'
 //@ts-ignore
 const SampleControls = () => {
@@ -9,12 +10,18 @@ const SampleControls = () => {
 	function handleHeatMap() {
 		heatMapData.features.forEach((f) => {
 			//@ts-ignore
-			f.geometry['newCoordinates'] = []
-			f.geometry.coordinates[0][0].forEach((co) => {
-				//@ts-ignore
-				f.geometry['newCoordinates'].push({ lat: co[1], lng: co[0] })
-			})
+
+			// f.geometry.coordinates[0][0].forEach((co) => {
+			// 	//@ts-ignore
+			// 	f.geometry['coordinates'] ={ lat: co[1], lng: co[0] })
+			// })
+			f.geometry['newCoordinates'] = f.geometry.coordinates[0][0].map((co) => ({
+				lat: co[1],
+				lng: co[0],
+			}))
 		})
+		//@ts-ignore
+		const arr = []
 
 		heatMapData.features.forEach((f) => {
 			//@ts-ignore
@@ -25,12 +32,28 @@ const SampleControls = () => {
 				]),
 			})
 			map?.data?.setStyle({
-				fillColor: 'transparent',
+				fillColor: 'green',
 				strokeWeight: 1,
 				strokeColor: 'purple',
 			})
+			console.log('geometry', f.geometry)
+
+			//@ts-ignore
+			let centroid = turf.centroid(f.geometry)
+			console.log('centroid', centroid)
+			const item = new google.maps.LatLng(
+				centroid.geometry.coordinates[1],
+				centroid.geometry.coordinates[0],
+			)
+			arr.push(item)
 		})
 
+		// let bbox = turf.bbox(heatMapData)
+		// map.fitBounds(bounds)
+		const bounds = new google.maps.LatLngBounds()
+		//@ts-ignore
+		arr.forEach((ele) => bounds.extend(ele))
+		map.panToBounds(bounds)
 		console.log('map', { ...{ ...map } })
 
 		console.log('heatMapData', heatMapData)
